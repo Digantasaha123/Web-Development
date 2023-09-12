4598d544b4a3bde5b85ad23964d6c49c3ac3bb60
Diganta123 <diganta15-4898@diu.edu.bd>
2023-09-12 23:08:50 +0600

    audio video

---
+++ Milestone4/Module 17 js/operations.js
@@ -0,0 +1,5 @@
+var onionPrice = 100;
+var eggprice= 150;
+console.log(onionPrice);
+console.log(eggprice);
+console.log(eggprice + onionPrice);
---
+++ Milestone4/Module 17 js/variables.js
@@ -0,0 +1,6 @@
+var price = 69.59;
+var size = 9;
+var person = "Sodor Uddin";
+var special = "Alia bhatt";
+var serious = true;
+isSingle = true;
